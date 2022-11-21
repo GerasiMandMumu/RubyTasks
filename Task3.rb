ac2fbc1091296b2dc2da47af4ced2fbe3cd3248e
GerasiMandMumu <konst.02@yandex.ru>
2022-11-21 15:41:32 +0300

    Добавил 3 задание

---
+++ Task3.rb
@@ -0,0 +1,5 @@
+el1 = true and false
+el2 = true && false
+
+puts el1 # true, т.к. будет  выполнено (el1 = true) and false, т.е. в переменной всегда будет первый операнд
+puts el2 # false, т.к. Истина И Ложь = Ложь
